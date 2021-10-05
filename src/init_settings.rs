353cd5c14771923897c880c0e39cc3c2f0ddac4f
l0calh05t <l0calh05t@gmx.net>
2021-10-05 16:25:08 +0200

    Document InitSettings

--- src/init_settings.rs
+++ src/init_settings.rs
@@ -1,13 +1,20 @@
 use std::ops::{Deref, DerefMut};
 
+/// Internal wrapper for `bela_sys::BelaInitSettings` implementing
+/// `Deref` and `DerefMut` with `Target = bela_sys::BelaInitSettings`.
 #[cfg(feature = "static")]
 pub(crate) struct InitSettings(bela_sys::BelaInitSettings);
 
+/// Internal wrapper for `bela_sys::BelaInitSettings` implementing
+/// `Deref` and `DerefMut` with `Target = bela_sys::BelaInitSettings`.
 #[cfg(not(feature = "static"))]
 pub(crate) struct InitSettings(*mut bela_sys::BelaInitSettings);
 
 impl Default for InitSettings {
     fn default() -> InitSettings {
+        // When linking against the static Bela library, InitSettings
+        // contains a bela_sys::BelaInitSettings directly, initialized
+        // using bela_sys::Bela_defaultSettings.
         #[cfg(feature = "static")]
         let settings = unsafe {
             let mut settings = std::mem::MaybeUninit::<bela_sys::BelaInitSettings>::uninit();
@@ -15,9 +22,14 @@ impl Default for InitSettings {
             settings.assume_init()
         };
 
+        // When linking against the dynamic Bela library, InitSettings
+        // contains a pointer to bela_sys::BelaInitSettings, allocated
+        // via bela_sys::Bela_InitSettings_alloc and initialized using
+        // bela_sys::Bela_defaultSettings for ABI compatibility.
         #[cfg(not(feature = "static"))]
         let settings = unsafe {
             let settings = bela_sys::Bela_InitSettings_alloc();
+            assert!(!settings.is_null());
             bela_sys::Bela_defaultSettings(settings);
             settings
         };
@@ -29,6 +41,9 @@ impl Default for InitSettings {
 #[cfg(not(feature = "static"))]
 impl Drop for InitSettings {
     fn drop(&mut self) {
+        // When linking against the dynamic Bela library, the settings
+        // object allocated in Default::default, must be freed using
+        // bela_sys::Bela_InitSettings_free
         unsafe {
             bela_sys::Bela_InitSettings_free(self.0);
         }
